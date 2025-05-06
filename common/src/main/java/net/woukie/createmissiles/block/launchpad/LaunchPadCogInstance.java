dc3bbf5e5a72be326e742f5f782d3b78504196a2
Woukie <lucaseldon@live.com>
2025-05-06 20:44:10 +0100

    Removed dreg from testing

--- common/src/main/java/net/woukie/createmissiles/block/launchpad/LaunchPadCogInstance.java
+++ common/src/main/java/net/woukie/createmissiles/block/launchpad/LaunchPadCogInstance.java
@@ -1,7 +1,6 @@
 package net.woukie.createmissiles.block.launchpad;
 
 import com.simibubi.create.content.kinetics.base.SingleRotatingInstance;
-import dev.engine_room.flywheel.api.visualization.VisualizationContext;
 import net.minecraft.core.Direction;
 import com.jozufozu.flywheel.api.Instancer;
 import com.jozufozu.flywheel.api.MaterialManager;
@@ -12,10 +11,6 @@ import com.simibubi.create.content.kinetics.base.flwdata.RotatingData;
 public class LaunchPadCogInstance extends SingleRotatingInstance<KineticBlockEntity> {
     public LaunchPadCogInstance(MaterialManager materialManager, KineticBlockEntity blockEntity) {
         super(materialManager, blockEntity);
-    }
-
-    public LaunchPadCogInstance(VisualizationContext visualizationContext, LaunchPadBlockEntity launchPadBlockEntity, float v) {
-
     }
 
     @Override
