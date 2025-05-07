53aba3899a5068d5ef9eeead23a2cf10713a3267
Woukie <lucaseldon@live.com>
2025-05-07 12:50:45 +0100

    Changed layout of items

--- common/src/main/java/net/woukie/createmissiles/registry/MissileItems.java
+++ common/src/main/java/net/woukie/createmissiles/registry/MissileItems.java
@@ -10,9 +10,17 @@ import net.woukie.createmissiles.item.WarheadSchematic;
 public class MissileItems {
     private static final CreateRegistrate REGISTRATE = CreateMissiles.registrate();
 
-    public static ItemEntry<WarheadSchematic> WARHEAD_SCHEMATIC = REGISTRATE.item(CreateMissiles.MOD_ID, "warhead_schematic", WarheadSchematic::new).register();
-    public static ItemEntry<ChassisSchematic> CHASSIS_SCHEMATIC = REGISTRATE.item(CreateMissiles.MOD_ID, "chassis_schematic", ChassisSchematic::new).register();
-    public static ItemEntry<ThrusterSchematic> THRUSTER_SCHEMATIC = REGISTRATE.item(CreateMissiles.MOD_ID, "thruster_schematic", ThrusterSchematic::new).register();
+    public static ItemEntry<WarheadSchematic> WARHEAD_SCHEMATIC = REGISTRATE
+            .item(CreateMissiles.MOD_ID, "warhead_schematic", WarheadSchematic::new)
+            .register();
+
+    public static ItemEntry<ChassisSchematic> CHASSIS_SCHEMATIC = REGISTRATE
+            .item(CreateMissiles.MOD_ID, "chassis_schematic", ChassisSchematic::new)
+            .register();
+
+    public static ItemEntry<ThrusterSchematic> THRUSTER_SCHEMATIC = REGISTRATE
+            .item(CreateMissiles.MOD_ID, "thruster_schematic",ThrusterSchematic::new)
+            .register();
 
     public static void init() {
         CreateMissiles.LOGGER.info("Registering items for " + CreateMissiles.NAME);
