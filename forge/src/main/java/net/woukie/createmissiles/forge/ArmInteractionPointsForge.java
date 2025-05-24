d450e9f96b467fb48fd394cb7491836023e33629
Woukie <lucaseldon@live.com>
2025-05-24 19:32:52 +0100

    Forge arm interaction implemented

--- forge/src/main/java/net/woukie/createmissiles/forge/ArmInteractionPointsForge.java
+++ forge/src/main/java/net/woukie/createmissiles/forge/ArmInteractionPointsForge.java
@@ -1,14 +1,22 @@
 package net.woukie.createmissiles.forge;
 
+import com.simibubi.create.content.kinetics.mechanicalArm.AllArmInteractionPointTypes;
 import com.simibubi.create.content.kinetics.mechanicalArm.ArmInteractionPoint;
 import com.simibubi.create.content.kinetics.mechanicalArm.ArmInteractionPointType;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.WorldlyContainer;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraftforge.items.IItemHandler;
+import net.minecraftforge.items.wrapper.SidedInvWrapper;
 import net.woukie.createmissiles.CreateMissiles;
+import net.woukie.createmissiles.block.launchpad.LaunchPadBlock;
 import net.woukie.createmissiles.registry.MissileBlocks;
 
+import javax.annotation.Nullable;
 import java.util.function.Function;
 
 public class ArmInteractionPointsForge {
@@ -36,11 +44,44 @@ public class ArmInteractionPointsForge {
         }
     }
 
-    public static class LaunchPadPoint extends ArmInteractionPoint {
+    public static class LaunchPadPoint extends AllArmInteractionPointTypes.DepositOnlyArmInteractionPoint {
         public LaunchPadPoint(ArmInteractionPointType type, Level level, BlockPos pos, BlockState state) {
             super(type, level, pos, state);
         }
 
+        @Override
+        public void updateCachedState() {
+            BlockState oldState = cachedState;
+            super.updateCachedState();
+            if (oldState != cachedState)
+                cachedHandler.invalidate();
+        }
+
+        @Nullable
+        @Override
+        protected IItemHandler getHandler() {
+            return null;
+        }
+
+        protected WorldlyContainer getContainer() {
+            LaunchPadBlock block = MissileBlocks.LAUNCH_PAD.get();
+            return block.getContainer(cachedState, level, pos);
+        }
+
+        @Override
+        public ItemStack insert(ItemStack stack, boolean simulate) {
+            ItemStack remainder = stack.copy();
+            ItemStack toInsert = remainder.split(1);
+            LaunchPadBlock block = MissileBlocks.LAUNCH_PAD.get();
+            WorldlyContainer container = block.getContainer(cachedState, level, pos);
+                if (!container.canPlaceItemThroughFace(0, toInsert, Direction.UP)) {
+                return stack;
+            }
+
+            IItemHandler handler = new SidedInvWrapper(getContainer(), Direction.UP);
+            handler.insertItem(0, toInsert, simulate);
+            return remainder;
+        }
     }
 
     public static void init() {
