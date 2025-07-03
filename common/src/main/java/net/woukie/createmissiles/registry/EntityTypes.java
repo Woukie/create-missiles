f8719f43db0a248cce023872e291b119a3d21c90
Woukie <lucaseldon@live.com>
2025-07-03 14:30:04 +0100

    Increased size of rocket bounding box

--- common/src/main/java/net/woukie/createmissiles/registry/EntityTypes.java
+++ common/src/main/java/net/woukie/createmissiles/registry/EntityTypes.java
@@ -15,7 +15,7 @@ public class EntityTypes {
 
     public static final RegistrySupplier<EntityType<MissileEntity>> MISSILE = ENTITY_TYPES.register(
             "rocket_entity",
-            () -> EntityType.Builder.of(MissileEntity::new, MobCategory.MISC).noSummon().fireImmune().sized(0.5F, 0.5F).clientTrackingRange(4).updateInterval(20).build(new ResourceLocation(CreateMissiles.MOD_ID, "rocket_entity").toString())
+            () -> EntityType.Builder.of(MissileEntity::new, MobCategory.MISC).noSummon().fireImmune().sized(1.5F, 5F).clientTrackingRange(4).updateInterval(20).build(new ResourceLocation(CreateMissiles.MOD_ID, "rocket_entity").toString())
     );
 
     public static void init() {
