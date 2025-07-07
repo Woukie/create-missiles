c3d782f8cd1ca9c625484e13ee576d72241cfdac
Woukie <lucaseldon@live.com>
2025-07-07 14:21:23 +0100

    Added shulkerbox model

---
+++ common/src/main/java/net/woukie/createmissiles/missilemanager/parts/warheads/ShulkerBoxWarhead.java
@@ -0,0 +1,103 @@
+package net.woukie.createmissiles.missilemanager.parts.warheads;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.particles.ParticleTypes;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.ListTag;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.world.Container;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.Vec3;
+import net.woukie.createmissiles.CreateMissiles;
+import net.woukie.createmissiles.client.MissilePartModel;
+import net.woukie.createmissiles.client.models.warheads.FireworkWarheadModel;
+import net.woukie.createmissiles.missilemanager.Trajectory;
+import net.woukie.createmissiles.missilemanager.parts.WarheadType;
+import net.woukie.createmissiles.registry.Packets;
+import org.jetbrains.annotations.NotNull;
+import org.joml.Vector3f;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class ShulkerBoxWarhead extends WarheadType {
+    private final MissilePartModel model = new FireworkWarheadModel();
+
+    @Override
+    public int getWeight() {
+        return 5;
+    }
+
+    @Override
+    public void onDetonate(Trajectory trajectory, MinecraftServer server) {
+        var level = (ServerLevel) trajectory.getData().level;
+        if (level == null) return;
+        var impactPos = trajectory.getPosition((float)trajectory.getImpactTime());
+
+        level.explode(null, impactPos.x, impactPos.y, impactPos.z, 0, Level.ExplosionInteraction.BLOCK);
+
+        CompoundTag explosions = trajectory.getData().warheadData;
+
+        if (explosions == null || explosions.isEmpty()) {
+            var random = Random.from(new Random());
+            level.addParticle(ParticleTypes.POOF, impactPos.x, impactPos.y, impactPos.z, random.nextGaussian() * 0.05, 0.005, random.nextGaussian() * 0.05);
+        } else {
+            List<ServerPlayer> players = new ArrayList<>();
+            level.players().forEach(serverPlayer -> {
+                BlockPos blockPos = serverPlayer.blockPosition();
+                if (blockPos.closerToCenterThan(new Vec3(impactPos.x, impactPos.y, impactPos.z), 512.0)) {
+                    players.add(serverPlayer);
+                }
+            });
+
+            Vector3f vel = new Vector3f(0, 0, 0); // TODO: Replace with rocket velocity
+            Packets.EXPLODE_FIREWORK.sendToPlayers(players, new ExplodeFireworkMessage(impactPos.toVector3f(), vel, explosions));
+        }
+    }
+
+    @Override
+    public void onLaunch(Trajectory trajectory) {
+        var level = (ServerLevel) trajectory.getData().level;
+        if (level != null) {
+            var p = trajectory.getPosition(0);
+            level.playSound(null, p.x, p.y, p.z, SoundEvents.FIREWORK_ROCKET_LAUNCH, SoundSource.NEUTRAL, 1, 1);
+        }
+    }
+
+    @Override
+    public @NotNull MissilePartModel getModel() {
+        return model;
+    }
+
+    @Override
+    public CompoundTag writeData(Container container, CompoundTag data) {
+        ListTag explosions = new ListTag();
+
+        for (int i = 0; i < container.getContainerSize(); i++) {
+            ItemStack itemStack = container.getItem(i);
+            if (itemStack.is(Items.FIREWORK_STAR) && itemStack.getTag() != null)
+                explosions.add(itemStack.getTagElement("Explosion"));
+        }
+        data.put("Explosions", explosions);
+        return data;
+    }
+
+    @Override
+    public ResourceLocation getResourceLocation() {
+        return new ResourceLocation(CreateMissiles.MOD_ID, "firework_warhead");
+    }
+
+    @Override
+    public Component getDisplayName() {
+        return Component.translatable("warheads.createmissiles.firework_warhead");
+    }
+}
