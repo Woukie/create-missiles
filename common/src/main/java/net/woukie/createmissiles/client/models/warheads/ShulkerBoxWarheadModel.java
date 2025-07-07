edcf2fcbbdfcc69485ec3889c112824bea9defcf
Woukie <lucaseldon@live.com>
2025-07-07 14:20:47 +0100

    Fixed warheadmodel attachments

---
+++ common/src/main/java/net/woukie/createmissiles/client/models/warheads/ShulkerBoxWarheadModel.java
@@ -0,0 +1,76 @@
+package net.woukie.createmissiles.client.models.warheads;
+
+import net.minecraft.client.model.geom.PartPose;
+import net.minecraft.client.model.geom.builders.*;
+import net.minecraft.resources.ResourceLocation;
+import net.woukie.createmissiles.CreateMissiles;
+import net.woukie.createmissiles.client.MissilePartModel;
+import org.joml.Vector3f;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+
+public class ShulkerBoxWarheadModel implements MissilePartModel {
+	private final List<Map<String, Vector3f>> attachments = List.of(
+			new HashMap<>() {{
+				put("bottom", new Vector3f(0, 0, 0));
+				put("top", new Vector3f(0, 0, 0));
+			}},
+			new HashMap<>() {{
+				put("bottom", new Vector3f(0, 0, 0));
+				put("top", new Vector3f(0, 5, 0));
+			}},
+			new HashMap<>() {{
+				put("bottom", new Vector3f(0, 0, 0));
+				put("top", new Vector3f(0, 10, 0));
+			}},
+			new HashMap<>() {{
+				put("bottom", new Vector3f(0, 0, 0));
+				put("top", new Vector3f(0, 10, 0));
+			}}
+	);
+
+	private final List<Function<PartDefinition, PartDefinition>> layers = List.of(
+			partDefinition -> partDefinition.addOrReplaceChild("bb_main", CubeListBuilder.create(), PartPose.ZERO),
+			partDefinition -> {
+				PartDefinition bb_main = partDefinition.addOrReplaceChild("bb_main", CubeListBuilder.create().texOffs(0, 20).addBox(-5.0F, 0.0F, -5.0F, 10.0F, 2.0F, 10.0F, new CubeDeformation(0.0F))
+						.texOffs(40, 0).addBox(-3.0F, 2.0F, -5.0F, 6.0F, 3.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 0.0F, 0.0F));
+				bb_main.addOrReplaceChild("cube_r1", CubeListBuilder.create().texOffs(40, 0).addBox(-3.0F, 0.0F, -5.0F, 6.0F, 3.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, 2.0F, 0.0F, 0.0F, 1.5708F, 0.0F));
+				bb_main.addOrReplaceChild("cube_r2", CubeListBuilder.create().texOffs(40, 0).addBox(-3.0F, 0.0F, -5.0F, 6.0F, 3.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, 2.0F, 0.0F, -3.1416F, 0.0F, 3.1416F));
+				bb_main.addOrReplaceChild("cube_r3", CubeListBuilder.create().texOffs(40, 0).addBox(-3.0F, 0.0F, -5.0F, 6.0F, 3.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, 2.0F, 0.0F, 0.0F, -1.5708F, 0.0F));
+				return partDefinition;
+			},
+			partDefinition -> partDefinition.addOrReplaceChild("bb_main", CubeListBuilder.create().texOffs(0, 32).addBox(-5.0F, 0.0F, -5.0F, 10.0F, 10.0F, 10.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 0.0F, 0.0F)),
+			partDefinition -> {
+				PartDefinition bb_main = partDefinition.addOrReplaceChild("bb_main", CubeListBuilder.create().texOffs(0, 0).addBox(-5.0F, 0.0F, -5.0F, 10.0F, 10.0F, 10.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 0.0F, 0.0F));
+				bb_main.addOrReplaceChild("ribon2_r1", CubeListBuilder.create().texOffs(40, 9).addBox(-1.0F, 0.0F, 0.0F, 2.0F, 1.0F, 4.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, 10.0F, 0.0F, 3.0543F, -0.3927F, -3.1416F));
+				bb_main.addOrReplaceChild("ribon1_r1", CubeListBuilder.create().texOffs(40, 4).addBox(-1.0F, 0.0F, 0.0F, 2.0F, 1.0F, 4.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, 10.0F, 0.0F, -0.0873F, -0.7854F, 0.0F));
+				return partDefinition;
+			}
+	);
+
+	@Override
+	public Map<String, Vector3f> getAttachements(int layer) {
+		return attachments.get(layer);
+	}
+
+	@Override
+	public LayerDefinition getLayerDefinition(int stage) {
+		MeshDefinition meshdefinition = new MeshDefinition();
+		PartDefinition partdefinition = meshdefinition.getRoot();
+		layers.get(stage).apply(partdefinition);
+		return LayerDefinition.create(meshdefinition, 64, 64);
+	}
+
+	@Override
+	public ResourceLocation getTexture(int stage) {
+		return new ResourceLocation(CreateMissiles.MOD_ID,"textures/entity/firework_warhead.png");
+	}
+
+	@Override
+	public int getStageCount() {
+		return layers.size();
+	}
+}
