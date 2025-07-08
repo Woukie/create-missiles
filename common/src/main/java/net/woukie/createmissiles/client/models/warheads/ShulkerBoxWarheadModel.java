922afbf6fafed0f9e6f821509fff0258cbb28e9e
Woukie <lucaseldon@live.com>
2025-07-08 14:33:38 +0100

    Updated shulker box model

--- common/src/main/java/net/woukie/createmissiles/client/models/warheads/ShulkerBoxWarheadModel.java
+++ common/src/main/java/net/woukie/createmissiles/client/models/warheads/ShulkerBoxWarheadModel.java
@@ -36,11 +36,11 @@ public class ShulkerBoxWarheadModel implements MissilePartModel {
 			partDefinition -> partDefinition.addOrReplaceChild("bb_main", CubeListBuilder.create(), PartPose.ZERO),
 			partDefinition -> {
 				PartDefinition bb_main = partDefinition.addOrReplaceChild("bb_main", CubeListBuilder.create(), PartPose.offset(0.0F, 0.0F, 0.0F));
-				bb_main.addOrReplaceChild("cube_r1", CubeListBuilder.create().texOffs(40, 0).addBox(-3.0F, -1.5F, -0.5F, 6.0F, 3.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(-4.5F, 3.5F, 0.0F, 0.0F, -1.5708F, -3.1416F));
-				bb_main.addOrReplaceChild("cube_r2", CubeListBuilder.create().texOffs(40, 0).addBox(-3.0F, -1.5F, -0.5F, 6.0F, 3.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, 3.5F, 4.5F, 3.1416F, 0.0F, 0.0F));
-				bb_main.addOrReplaceChild("cube_r3", CubeListBuilder.create().texOffs(40, 0).addBox(-3.0F, -1.5F, -0.5F, 6.0F, 3.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, 3.5F, -4.5F, 0.0F, 0.0F, 3.1416F));
-				bb_main.addOrReplaceChild("cube_r4", CubeListBuilder.create().texOffs(40, 0).addBox(-3.0F, -1.5F, -0.5F, 6.0F, 3.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(4.5F, 3.5F, 0.0F, 0.0F, 1.5708F, 3.1416F));
-				bb_main.addOrReplaceChild("cube_r5", CubeListBuilder.create().texOffs(0, 20).addBox(-5.0F, -1.0F, -5.0F, 10.0F, 2.0F, 10.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, 1.0F, 0.0F, 3.1416F, 0.0F, 0.0F));
+				bb_main.addOrReplaceChild("cube_r1", CubeListBuilder.create().texOffs(40, 0).addBox(-3.0F, -1.5F, -0.5F, 6.0F, 3.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(-4.5F, 3.525F, 0.0F, 0.0F, -1.5708F, -3.1416F));
+				bb_main.addOrReplaceChild("cube_r2", CubeListBuilder.create().texOffs(40, 0).addBox(-3.0F, -1.5F, -0.5F, 6.0F, 3.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, 3.525F, 4.5F, 3.1416F, 0.0F, 0.0F));
+				bb_main.addOrReplaceChild("cube_r3", CubeListBuilder.create().texOffs(40, 0).addBox(-3.0F, -1.5F, -0.5F, 6.0F, 3.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, 3.525F, -4.5F, 0.0F, 0.0F, 3.1416F));
+				bb_main.addOrReplaceChild("cube_r4", CubeListBuilder.create().texOffs(40, 0).addBox(-3.0F, -1.5F, -0.5F, 6.0F, 3.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(4.5F, 3.525F, 0.0F, 0.0F, 1.5708F, 3.1416F));
+				bb_main.addOrReplaceChild("cube_r5", CubeListBuilder.create().texOffs(0, 20).addBox(-5.0F, -1.0F, -5.0F, 10.0F, 2.0F, 10.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, 1.025F, 0.0F, 3.1416F, 0.0F, 0.0F));
 				return partDefinition;
 			},
 			partDefinition -> {
@@ -50,8 +50,8 @@ public class ShulkerBoxWarheadModel implements MissilePartModel {
 			},
 			partDefinition -> {
 				PartDefinition bb_main = partDefinition.addOrReplaceChild("bb_main", CubeListBuilder.create(), PartPose.offset(0.0F, 0.0F, 0.0F));
-				bb_main.addOrReplaceChild("ribon2_r1", CubeListBuilder.create().texOffs(40, 9).addBox(-1.0F, 0.0F, 0.0F, 2.0F, 1.0F, 4.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, 10.0F, 0.0F, 3.0543F, -0.3927F, -3.1416F));
-				bb_main.addOrReplaceChild("ribon1_r1", CubeListBuilder.create().texOffs(40, 4).addBox(-1.0F, 0.0F, 0.0F, 2.0F, 1.0F, 4.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, 10.0F, 0.0F, -0.0873F, -0.7854F, 0.0F));
+				bb_main.addOrReplaceChild("ribon2_r1", CubeListBuilder.create().texOffs(40, 9).addBox(-1.0F, 0.0F, 0.0F, 2.0F, 1.0F, 4.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, 10.0F, 0.0F, 2.6616F, -0.3927F, -3.1416F));
+				bb_main.addOrReplaceChild("ribon1_r1", CubeListBuilder.create().texOffs(40, 4).addBox(-1.0F, 0.0F, 0.0F, 2.0F, 1.0F, 4.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, 10.0F, 0.0F, -0.48F, -0.7854F, 0.0F));
 				bb_main.addOrReplaceChild("box_r1", CubeListBuilder.create().texOffs(0, 0).addBox(-5.0F, -5.0F, -5.0F, 10.0F, 10.0F, 10.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, 5.0F, 0.0F, 0.0F, 0.0F, -3.1416F));
 				return partDefinition;
 			}
