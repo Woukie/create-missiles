a45ad2b284db4f95ba701bb447c8135cd7cb1e7c
Woukie <lucaseldon@live.com>
2025-09-02 19:18:12 +0100

    Initial withered warhead

--- common/src/main/java/net/woukie/createmissiles/registry/PartTypes.java
+++ common/src/main/java/net/woukie/createmissiles/registry/PartTypes.java
@@ -36,6 +36,7 @@ public class PartTypes {
     public static void init() {
         CreateMissiles.LOGGER.info("Registering missile part types for " + CreateMissiles.NAME);
 
+        PartTypes.register(new WitheredWarhead());
         PartTypes.register(new FrozenWarhead());
         PartTypes.register(new AnnoyingWarhead());
         PartTypes.register(new DirectHitWarhead());
