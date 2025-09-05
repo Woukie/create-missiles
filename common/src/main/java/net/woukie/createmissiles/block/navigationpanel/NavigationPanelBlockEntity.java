9ee62f6ab9185050d9eddd84fa8ba8f961557b69
Jamie Markiewicz <jmarkiewicz88@outlook.com>
2025-09-05 19:27:05 +0100

    Increased missile client tracking range.

--- common/src/main/java/net/woukie/createmissiles/block/navigationpanel/NavigationPanelBlockEntity.java
+++ common/src/main/java/net/woukie/createmissiles/block/navigationpanel/NavigationPanelBlockEntity.java
@@ -44,7 +44,6 @@ public class NavigationPanelBlockEntity extends AbstractBasicBlockEntity {
     private BlockPos target;
     private final ContainerData dataAccess;
     private AssemblyPanelBlockEntity assemblyPanel;
-    private int relativeMinThrustDuration = 0;
 
     public NavigationPanelBlockEntity(BlockEntityType<?> blockEntityType, BlockPos blockPos, BlockState blockState) {
         super(blockEntityType, blockPos, blockState);
@@ -76,7 +75,6 @@ public class NavigationPanelBlockEntity extends AbstractBasicBlockEntity {
                             getLevel(),
                             BlockEntities.ASSEMBLY_PANEL.get()
                     ) == null ? 0 : 1;
-                    case 12 -> relativeMinThrustDuration;
                     default -> 0;
                 };
             }
@@ -84,7 +82,7 @@ public class NavigationPanelBlockEntity extends AbstractBasicBlockEntity {
             public void set(int i, int j) {}
 
             public int getCount() {
-                return 13;
+                return 12;
             }
         };
     }
@@ -127,11 +125,10 @@ public class NavigationPanelBlockEntity extends AbstractBasicBlockEntity {
         double mass = warheadType.getMass() + chassisType.getMass() + thrusterType.getMass();
         double targetDistance = Vector3d.distance(target.getX(), 0, target.getZ(), this.getBlockPos().getX(), 0, this.getBlockPos().getZ());
 
-        TrajectoryHelper.LaunchSolution solution = findMinLaunchSolution(targetDistance, thrusterType.getThrust(), 50, 30, 90, mass);
+        TrajectoryHelper.LaunchSolution solution = findMinLaunchSolution(targetDistance, thrusterType.getThrust(), 50, 45, 90, mass);
         if(solution != null)
         {
             System.out.println(solution.thrustDuration);
-            relativeMinThrustDuration = (int)(solution.thrustDuration / (chassisType.getFuelCapacity() / thrusterType.getBurnRate()) * 100);
         }else
         {
             System.out.println("No Solution");
