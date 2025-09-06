95b0f35970442e1ee7246be9746b9fd104f389aa
Woukie <lucaseldon@live.com>
2025-09-06 14:17:59 +0100

    Guardian last stage model

--- common/src/main/java/net/woukie/createmissiles/client/models/warheads/GuardianWarheadModel.java
+++ common/src/main/java/net/woukie/createmissiles/client/models/warheads/GuardianWarheadModel.java
@@ -46,17 +46,37 @@ public class GuardianWarheadModel implements MissilePartModel {
 				return bb_main;
 			},
 			partDefinition -> {
-				PartDefinition bb_main = partDefinition.addOrReplaceChild("bb_main", CubeListBuilder.create(), PartPose.offset(0.0F, 0.0F, 0.0F));
-				bb_main.addOrReplaceChild("plate_r1", CubeListBuilder.create().texOffs(0, 12).addBox(-1.5F, -0.5F, -3.5F, 3.0F, 1.0F, 7.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, 0.5F, 0.0F, 3.1416F, 0.7854F, 0.0F));
-				bb_main.addOrReplaceChild("cube_r1", CubeListBuilder.create().texOffs(26, 20).addBox(-1.5F, -2.5F, 0.0F, 3.0F, 5.0F, 0.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(2.5456F, 2.5F, 2.5456F, -3.1416F, -0.7854F, 3.1416F));
-				bb_main.addOrReplaceChild("cube_r2", CubeListBuilder.create().texOffs(26, 23).addBox(-1.5F, -1.0F, 0.0F, 3.0F, 2.0F, 0.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(2.0679F, 5.7373F, 2.0679F, -2.3998F, -0.7854F, -3.1416F));
-				bb_main.addOrReplaceChild("cube_r3", CubeListBuilder.create().texOffs(20, 20).addBox(-1.5F, -3.0F, 0.0F, 3.0F, 6.0F, 0.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(2.5456F, 3.0F, 2.5456F, 0.0F, -1.5708F, 0.0F));
-				bb_main.addOrReplaceChild("cube_r4", CubeListBuilder.create().texOffs(26, 23).addBox(-1.5F, -1.0F, 0.0F, 3.0F, 2.0F, 0.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(-2.0679F, 5.7373F, -2.0679F, 2.3998F, -0.7854F, 3.1416F));
-				bb_main.addOrReplaceChild("cube_r5", CubeListBuilder.create().texOffs(20, 20).addBox(-1.5F, -3.0F, 0.0F, 3.0F, 6.0F, 0.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(-2.5456F, 3.0F, -2.5456F, 0.0F, -1.5708F, 0.0F));
-				bb_main.addOrReplaceChild("cube_r6", CubeListBuilder.create().texOffs(26, 20).addBox(-1.5F, -2.5F, 0.0F, 3.0F, 5.0F, 0.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(-2.5456F, 2.5F, -2.5456F, -3.1416F, -0.7854F, 3.1416F));
-				bb_main.addOrReplaceChild("insides_r1", CubeListBuilder.create().texOffs(0, 20).addBox(-2.5F, -2.5F, -2.5F, 5.0F, 5.0F, 5.0F, new CubeDeformation(0.0F))
-						.texOffs(0, 0).addBox(-3.0F, -3.0F, -3.0F, 6.0F, 6.0F, 6.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, 3.6F, 0.0F, 3.1416F, 0.7854F, 0.0F));
-				return bb_main;
+				PartDefinition guardian = partDefinition.addOrReplaceChild("guardian", CubeListBuilder.create().texOffs(12, 2).addBox(-1.0F, -1.0F, -1.6563F, 2.0F, 2.0F, 2.0F, new CubeDeformation(0.0F))
+						.texOffs(14, 2).addBox(-0.5F, -0.5F, -2.1563F, 1.0F, 1.0F, 1.0F, new CubeDeformation(0.0F))
+						.texOffs(15, -1).addBox(0.0F, -1.0F, -2.6563F, 0.0F, 2.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.2431F, 3.0F, -0.2431F, 0.0F, -0.7854F, 0.0F));
+				PartDefinition Spike12_r1 = guardian.addOrReplaceChild("Spike12_r1", CubeListBuilder.create().texOffs(14, 0).addBox(-0.5F, -0.5F, -0.5F, 1.0F, 1.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(-1.1464F, 0.0F, 1.4902F, 0.0F, 0.7854F, 0.0F));
+				PartDefinition Spike11_r1 = guardian.addOrReplaceChild("Spike11_r1", CubeListBuilder.create().texOffs(14, 0).addBox(-0.5F, -0.5F, -0.5F, 1.0F, 1.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, -1.1464F, -0.8027F, 0.7854F, 0.0F, 0.0F));
+				PartDefinition Spike10_r1 = guardian.addOrReplaceChild("Spike10_r1", CubeListBuilder.create().texOffs(14, 0).addBox(-0.5F, -0.5F, -0.5F, 1.0F, 1.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, 1.1464F, -0.8027F, -0.7854F, 0.0F, 0.0F));
+				PartDefinition Spike9_r1 = guardian.addOrReplaceChild("Spike9_r1", CubeListBuilder.create().texOffs(14, 0).addBox(-0.5F, -0.5F, -0.5F, 1.0F, 1.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, 1.1464F, 1.4902F, 0.7854F, 0.0F, 0.0F));
+				PartDefinition Spike8_r1 = guardian.addOrReplaceChild("Spike8_r1", CubeListBuilder.create().texOffs(14, 0).addBox(-0.5F, -0.5F, -0.5F, 1.0F, 1.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, -1.1464F, 1.4902F, -0.7854F, 0.0F, 0.0F));
+				PartDefinition Spike7_r1 = guardian.addOrReplaceChild("Spike7_r1", CubeListBuilder.create().texOffs(14, 0).addBox(-0.5F, -0.5F, -0.5F, 1.0F, 1.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(-1.1464F, -1.1464F, 0.3437F, 0.0F, 0.0F, -0.7854F));
+				PartDefinition Spike6_r1 = guardian.addOrReplaceChild("Spike6_r1", CubeListBuilder.create().texOffs(14, 0).addBox(-0.5F, -0.5F, -0.5F, 1.0F, 1.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(1.1464F, -1.1464F, 0.3437F, 0.0F, 0.0F, 0.7854F));
+				PartDefinition Spike5_r1 = guardian.addOrReplaceChild("Spike5_r1", CubeListBuilder.create().texOffs(14, 0).addBox(-0.5F, -0.5F, -0.5F, 1.0F, 1.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(1.1464F, 1.1464F, 0.3437F, 0.0F, 0.0F, -0.7854F));
+				PartDefinition Spike4_r1 = guardian.addOrReplaceChild("Spike4_r1", CubeListBuilder.create().texOffs(14, 0).addBox(-0.5F, -0.5F, -0.5F, 1.0F, 1.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(-1.1464F, 1.1464F, 0.3437F, 0.0F, 0.0F, 0.7854F));
+				PartDefinition Spike3_r1 = guardian.addOrReplaceChild("Spike3_r1", CubeListBuilder.create().texOffs(14, 0).addBox(-0.5F, -0.5F, -0.5F, 1.0F, 1.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(-1.1464F, 0.0F, -0.8027F, 0.0F, -0.7854F, 0.0F));
+				PartDefinition Spike2_r1 = guardian.addOrReplaceChild("Spike2_r1", CubeListBuilder.create().texOffs(14, 0).addBox(-0.5F, -0.5F, -0.5F, 1.0F, 1.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(1.1464F, 0.0F, -0.8027F, 0.0F, 0.7854F, 0.0F));
+				PartDefinition Spike1_r1 = guardian.addOrReplaceChild("Spike1_r1", CubeListBuilder.create().texOffs(14, 0).addBox(-0.5F, -0.5F, -0.5F, 1.0F, 1.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(1.1464F, 0.0F, 1.4902F, 0.0F, -0.7854F, 0.0F));
+				PartDefinition Body_r1 = guardian.addOrReplaceChild("Body_r1", CubeListBuilder.create().texOffs(0, 0).addBox(-1.5F, -1.5F, -1.5F, 3.0F, 3.0F, 3.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, 0.0F, 0.3437F, 0.0F, 0.0F, -3.1416F));
+				PartDefinition bowl = partDefinition.addOrReplaceChild("bowl", CubeListBuilder.create(), PartPose.offset(0.0F, 2.4736F, 0.0F));
+				PartDefinition cube_r1 = bowl.addOrReplaceChild("cube_r1", CubeListBuilder.create().texOffs(30, 2).addBox(-2.0F, -1.0F, -0.5F, 4.0F, 2.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(2.3536F, 0.9656F, -2.3536F, -2.3562F, 0.0F, -1.5708F));
+				PartDefinition cube_r2 = bowl.addOrReplaceChild("cube_r2", CubeListBuilder.create().texOffs(30, 2).addBox(-2.0F, -1.0F, -0.5F, 4.0F, 2.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(-2.3536F, 0.9656F, -2.3536F, 2.3562F, 0.0F, -1.5708F));
+				PartDefinition cube_r3 = bowl.addOrReplaceChild("cube_r3", CubeListBuilder.create().texOffs(30, 2).addBox(-2.0F, -1.0F, -0.5F, 4.0F, 2.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(-2.3536F, 0.9656F, 2.3536F, 0.7854F, 0.0F, -1.5708F));
+				PartDefinition cube_r4 = bowl.addOrReplaceChild("cube_r4", CubeListBuilder.create().texOffs(30, 2).addBox(-2.0F, -1.0F, -0.5F, 4.0F, 2.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(2.3536F, 0.9656F, 2.3536F, -0.7854F, 0.0F, -1.5708F));
+				PartDefinition cube_r5 = bowl.addOrReplaceChild("cube_r5", CubeListBuilder.create().texOffs(30, 2).addBox(-2.0F, -1.0F, -0.5F, 4.0F, 2.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(2.3536F, -1.4129F, -0.025F, 0.0F, -1.5708F, 2.3562F));
+				PartDefinition cube_r6 = bowl.addOrReplaceChild("cube_r6", CubeListBuilder.create().texOffs(30, 2).addBox(-2.0F, -1.0F, -0.5F, 4.0F, 2.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(-2.3536F, -1.4129F, 0.025F, 0.0F, 1.5708F, -2.3562F));
+				PartDefinition cube_r7 = bowl.addOrReplaceChild("cube_r7", CubeListBuilder.create().texOffs(30, 2).addBox(-2.0F, -1.0F, -0.5F, 4.0F, 2.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(-0.025F, -1.4129F, -2.3536F, 2.3562F, 0.0F, 0.0F));
+				PartDefinition cube_r8 = bowl.addOrReplaceChild("cube_r8", CubeListBuilder.create().texOffs(30, 2).addBox(-2.0F, -1.0F, -0.5F, 4.0F, 2.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.025F, -1.4129F, 2.3536F, -0.7854F, 0.0F, -3.1416F));
+				PartDefinition cube_r9 = bowl.addOrReplaceChild("cube_r9", CubeListBuilder.create().texOffs(20, 0).addBox(-2.0F, -2.0F, -0.5F, 4.0F, 4.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, -1.9736F, 0.0F, 1.5708F, 0.0F, 0.0F));
+				PartDefinition cube_r10 = bowl.addOrReplaceChild("cube_r10", CubeListBuilder.create().texOffs(20, 0).addBox(-2.0F, -2.0F, -0.5F, 4.0F, 4.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(-2.9142F, 0.9406F, 0.0F, 0.0F, 1.5708F, 3.1416F));
+				PartDefinition cube_r11 = bowl.addOrReplaceChild("cube_r11", CubeListBuilder.create().texOffs(20, 0).addBox(-2.0F, -2.0F, -0.5F, 4.0F, 4.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(2.9142F, 0.9406F, 0.0F, 0.0F, -1.5708F, -3.1416F));
+				PartDefinition cube_r12 = bowl.addOrReplaceChild("cube_r12", CubeListBuilder.create().texOffs(20, 0).addBox(-2.0F, -2.0F, -0.5F, 4.0F, 4.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, 0.9406F, -2.9142F, -3.1416F, 0.0F, 0.0F));
+				PartDefinition cube_r13 = bowl.addOrReplaceChild("cube_r13", CubeListBuilder.create().texOffs(20, 0).addBox(-2.0F, -2.0F, -0.5F, 4.0F, 4.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, 0.9406F, 2.9142F, 0.0F, 0.0F, -3.1416F));
+				return guardian;
 			}
 	);
 
@@ -70,12 +90,12 @@ public class GuardianWarheadModel implements MissilePartModel {
 		MeshDefinition meshdefinition = new MeshDefinition();
 		PartDefinition partdefinition = meshdefinition.getRoot();
 		layers.get(stage).apply(partdefinition);
-		return LayerDefinition.create(meshdefinition, 72, 32);
+		return LayerDefinition.create(meshdefinition, 40, 15);
 	}
 
 	@Override
 	public ResourceLocation getTexture(int stage) {
-		return new ResourceLocation(CreateMissiles.MOD_ID,"textures/entity/flaming_warhead.png");
+		return new ResourceLocation(CreateMissiles.MOD_ID,"textures/entity/guardian_warhead.png");
 	}
 
 	@Override
