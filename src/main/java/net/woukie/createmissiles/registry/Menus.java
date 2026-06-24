4c2d0bc266889b9c708a903670f2542da3cae01e
Woukie <lucaseldon@live.com>
2026-06-24 11:27:48 +0100

    Suppliers to deferred holders menus

--- src/main/java/net/woukie/createmissiles/registry/Menus.java
+++ src/main/java/net/woukie/createmissiles/registry/Menus.java
@@ -3,6 +3,7 @@ package net.woukie.createmissiles.registry;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.world.flag.FeatureFlags;
 import net.minecraft.world.inventory.MenuType;
+import net.neoforged.neoforge.registries.DeferredHolder;
 import net.neoforged.neoforge.registries.DeferredRegister;
 import net.woukie.createmissiles.CreateMissiles;
 import net.woukie.createmissiles.inventory.ControlPanelMenu;
@@ -10,24 +11,22 @@ import net.woukie.createmissiles.inventory.DroneMenu;
 import net.woukie.createmissiles.inventory.NavigationPanelMenu;
 import net.woukie.createmissiles.inventory.AssemblyPanelMenu;
 
-import java.util.function.Supplier;
-
 public class Menus {
     public static final DeferredRegister<MenuType<?>> MENUS = DeferredRegister.create(BuiltInRegistries.MENU, CreateMissiles.MOD_ID);
 
-    public static final Supplier<MenuType<ControlPanelMenu>> CONTROL_PANEL = MENUS.register("control_panel", () -> new MenuType<>(ControlPanelMenu::new, FeatureFlags.VANILLA_SET));
+    public static final DeferredHolder<MenuType<?>, MenuType<ControlPanelMenu>> CONTROL_PANEL = MENUS.register("control_panel", () -> new MenuType<>(ControlPanelMenu::new, FeatureFlags.VANILLA_SET));
 
-    public static final Supplier<MenuType<AssemblyPanelMenu>> ASSEMBLY_PANEL = MENUS.register(
+    public static final DeferredHolder<MenuType<?>, MenuType<AssemblyPanelMenu>> ASSEMBLY_PANEL = MENUS.register(
             "assembly_panel",
             () -> new MenuType<>(AssemblyPanelMenu::new, FeatureFlags.VANILLA_SET)
     );
 
-    public static final Supplier<MenuType<NavigationPanelMenu>> NAVIGATION_PANEL = MENUS.register(
+    public static final DeferredHolder<MenuType<?>, MenuType<NavigationPanelMenu>> NAVIGATION_PANEL = MENUS.register(
             "navigation_panel",
             () -> new MenuType<>(NavigationPanelMenu::new, FeatureFlags.VANILLA_SET)
     );
 
-    public static final Supplier<MenuType<DroneMenu>> DRONE = MENUS.register(
+    public static final DeferredHolder<MenuType<?>, MenuType<DroneMenu>> DRONE = MENUS.register(
             "drone",
             () -> new MenuType<>(DroneMenu::new, FeatureFlags.VANILLA_SET)
     );
