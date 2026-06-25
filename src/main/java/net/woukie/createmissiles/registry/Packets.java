8416672d48ada8fb0e0e7430bbb09c476318ba1b
Woukie <lucaseldon@live.com>
2026-06-25 11:09:07 +0100

    Initial packet conversion

--- src/main/java/net/woukie/createmissiles/registry/Packets.java
+++ src/main/java/net/woukie/createmissiles/registry/Packets.java
@@ -1,7 +1,8 @@
 package net.woukie.createmissiles.registry;
 
-import dev.architectury.networking.NetworkChannel;
 import net.minecraft.resources.ResourceLocation;
+import net.neoforged.neoforge.network.event.RegisterPayloadHandlersEvent;
+import net.neoforged.neoforge.network.registration.PayloadRegistrar;
 import net.woukie.createmissiles.CreateMissiles;
 import net.woukie.createmissiles.block.controlpanel.messages.ClickLaunchMessage;
 import net.woukie.createmissiles.block.controlpanel.messages.TriggerBuildParticles;
@@ -13,25 +14,27 @@ import net.woukie.createmissiles.entity.drone.SendDroneMessage;
 import net.woukie.createmissiles.missiles.parts.warheads.messages.ExplodeFireworkMessage;
 
 public class Packets {
-    public static final NetworkChannel NAVIGATION_PANEL_CLICK_MAP = NetworkChannel.create(new ResourceLocation(CreateMissiles.MOD_ID, "navigation_panel_click_map"));
-    public static final NetworkChannel NAVIGATION_PANEL_CLICK_FUEL = NetworkChannel.create(new ResourceLocation(CreateMissiles.MOD_ID, "navigation_panel_click_fuel"));
-    public static final NetworkChannel CONTROL_PANEL_CLICK_LAUNCH = NetworkChannel.create(new ResourceLocation(CreateMissiles.MOD_ID, "control_panel_click_launch"));
-    public static final NetworkChannel EXPLODE_FIREWORK = NetworkChannel.create(new ResourceLocation(CreateMissiles.MOD_ID, "explode_firework"));
-    public static final NetworkChannel UPDATE_MAP_DATA = NetworkChannel.create(new ResourceLocation(CreateMissiles.MOD_ID, "update_map_data"));
-    public static final NetworkChannel SEND_DRONE = NetworkChannel.create(new ResourceLocation(CreateMissiles.MOD_ID, "send_drone"));
-    public static final NetworkChannel TRIGGER_BUILD_PARTICLES = NetworkChannel.create(new ResourceLocation(CreateMissiles.MOD_ID, "trigger_build_particles"));
-    public static final NetworkChannel CREATE_FLASH = NetworkChannel.create(new ResourceLocation(CreateMissiles.MOD_ID, "create_flash"));
+    public static final NetworkChannel NAVIGATION_PANEL_CLICK_MAP = NetworkChannel.create(ResourceLocation.fromNamespaceAndPath(CreateMissiles.MOD_ID, "navigation_panel_click_map"));
+    public static final NetworkChannel NAVIGATION_PANEL_CLICK_FUEL = NetworkChannel.create(ResourceLocation.fromNamespaceAndPath(CreateMissiles.MOD_ID, "navigation_panel_click_fuel"));
+    public static final NetworkChannel CONTROL_PANEL_CLICK_LAUNCH = NetworkChannel.create(ResourceLocation.fromNamespaceAndPath(CreateMissiles.MOD_ID, "control_panel_click_launch"));
+    public static final NetworkChannel EXPLODE_FIREWORK = NetworkChannel.create(ResourceLocation.fromNamespaceAndPath(CreateMissiles.MOD_ID, "explode_firework"));
+    public static final NetworkChannel UPDATE_MAP_DATA = NetworkChannel.create(ResourceLocation.fromNamespaceAndPath(CreateMissiles.MOD_ID, "update_map_data"));
+    public static final NetworkChannel SEND_DRONE = NetworkChannel.create(ResourceLocation.fromNamespaceAndPath(CreateMissiles.MOD_ID, "send_drone"));
+    public static final NetworkChannel TRIGGER_BUILD_PARTICLES = NetworkChannel.create(ResourceLocation.fromNamespaceAndPath(CreateMissiles.MOD_ID, "trigger_build_particles"));
+    public static final NetworkChannel CREATE_FLASH = NetworkChannel.create(ResourceLocation.fromNamespaceAndPath(CreateMissiles.MOD_ID, "create_flash"));
 
-    public static void init() {
-        CreateMissiles.LOGGER.info("Registering packet resource locations for " + CreateMissiles.NAME);
+    public static void onRegisterPayloadHandlers(RegisterPayloadHandlersEvent event) {
+        CreateMissiles.LOGGER.info("Registering payload handlers for " + CreateMissiles.NAME);
 
-        NAVIGATION_PANEL_CLICK_MAP.register(ClickMapMessage.class, ClickMapMessage::encode, ClickMapMessage::new, ClickMapMessage::apply);
-        NAVIGATION_PANEL_CLICK_FUEL.register(ClickFuelMessage.class, ClickFuelMessage::encode, ClickFuelMessage::new, ClickFuelMessage::apply);
-        CONTROL_PANEL_CLICK_LAUNCH.register(ClickLaunchMessage.class, ClickLaunchMessage::encode, ClickLaunchMessage::new, ClickLaunchMessage::apply);
-        EXPLODE_FIREWORK.register(ExplodeFireworkMessage.class, ExplodeFireworkMessage::encode, ExplodeFireworkMessage::new, ExplodeFireworkMessage::apply);
-        UPDATE_MAP_DATA.register(UpdateMapDataMessage.class, UpdateMapDataMessage::encode, UpdateMapDataMessage::new, UpdateMapDataMessage::apply);
-        SEND_DRONE.register(SendDroneMessage.class, SendDroneMessage::encode, SendDroneMessage::new, SendDroneMessage::apply);
-        TRIGGER_BUILD_PARTICLES.register(TriggerBuildParticles.class, TriggerBuildParticles::encode, TriggerBuildParticles::new, TriggerBuildParticles::apply);
-        CREATE_FLASH.register(CreateFlashMessage.class, CreateFlashMessage::encode, CreateFlashMessage::new, CreateFlashMessage::apply);
+        final PayloadRegistrar registrar = event.registrar("1");
+
+        registrar.playToServer(ClickMapMessage.TYPE, ClickMapMessage.STREAM_CODEC, ClickMapMessage::apply);
+        registrar.playToServer(ClickFuelMessage.TYPE, ClickFuelMessage.STREAM_CODEC, ClickFuelMessage::apply);
+        registrar.playToServer(ClickLaunchMessage.TYPE, ClickLaunchMessage.STREAM_CODEC, ClickLaunchMessage::apply);
+        registrar.playToServer(ExplodeFireworkMessage.TYPE, ExplodeFireworkMessage.STREAM_CODEC, ExplodeFireworkMessage::apply);
+        registrar.playToServer(UpdateMapDataMessage.TYPE, UpdateMapDataMessage.STREAM_CODEC, UpdateMapDataMessage::apply);
+        registrar.playToServer(SendDroneMessage.TYPE, SendDroneMessage.STREAM_CODEC, SendDroneMessage::apply);
+        registrar.playToServer(TriggerBuildParticles.TYPE, TriggerBuildParticles.STREAM_CODEC, TriggerBuildParticles::apply);
+        registrar.playToServer(CreateFlashMessage.TYPE, CreateFlashMessage.STREAM_CODEC, CreateFlashMessage::apply);
     }
 }
