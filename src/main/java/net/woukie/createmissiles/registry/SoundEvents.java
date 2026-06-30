21f1268b189e72c659a6b5455ff5ae6ab05d1afa
Woukie <lucaseldon@live.com>
2026-06-30 14:58:31 +0100

    Sound events converted

--- src/main/java/net/woukie/createmissiles/registry/SoundEvents.java
+++ src/main/java/net/woukie/createmissiles/registry/SoundEvents.java
@@ -1,35 +1,34 @@
 package net.woukie.createmissiles.registry;
 
 
-import dev.architectury.registry.registries.DeferredRegister;
-import dev.architectury.registry.registries.RegistrySupplier;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.sounds.SoundEvent;
+import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.neoforge.registries.DeferredHolder;
+import net.neoforged.neoforge.registries.DeferredRegister;
 import net.woukie.createmissiles.CreateMissiles;
 
 import static net.woukie.createmissiles.CreateMissiles.MOD_ID;
 
 public class SoundEvents {
-    public static final DeferredRegister<SoundEvent> SOUND_EVENTS =
-            DeferredRegister.create(MOD_ID, Registries.SOUND_EVENT);
+    public static final DeferredRegister<SoundEvent> SOUND_EVENTS = DeferredRegister.create(Registries.SOUND_EVENT, MOD_ID);
 
-    public static final RegistrySupplier<SoundEvent> DING = register("ding") ;
-    public static final RegistrySupplier<SoundEvent> BUILD = register("build") ;
-    public static final RegistrySupplier<SoundEvent> BUILD_SPECIAL = register("build_special") ;
-    public static final RegistrySupplier<SoundEvent> BUTTON = register("button") ;
-    public static final RegistrySupplier<SoundEvent> CRUNCH = register("crunch") ;
+    public static final DeferredHolder<SoundEvent, SoundEvent> DING = register("ding") ;
+    public static final DeferredHolder<SoundEvent, SoundEvent> BUILD = register("build") ;
+    public static final DeferredHolder<SoundEvent, SoundEvent> BUILD_SPECIAL = register("build_special") ;
+    public static final DeferredHolder<SoundEvent, SoundEvent> BUTTON = register("button") ;
+    public static final DeferredHolder<SoundEvent, SoundEvent> CRUNCH = register("crunch") ;
 
     public static void init() {
         CreateMissiles.LOGGER.info("Registering sound events for " + CreateMissiles.NAME);
-        SOUND_EVENTS.register();
+        SOUND_EVENTS.register(NeoForge.EVENT_BUS);
     }
 
-    private static RegistrySupplier<SoundEvent> register(String name) {
-        ResourceLocation location = new ResourceLocation(MOD_ID, name);
+    private static DeferredHolder<SoundEvent, SoundEvent> register(String name) {
         return SOUND_EVENTS.register(
-                location,
-                () -> SoundEvent.createVariableRangeEvent(location)
+                name,
+                () -> SoundEvent.createVariableRangeEvent(ResourceLocation.fromNamespaceAndPath(MOD_ID, name))
         );
     }
 }
